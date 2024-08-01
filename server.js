398a25ef57f4cddddb53a4f7bfa2eaddfaab3eed
bacancy-mehul <mehul.morker@bacancy.com>
2024-08-01 12:28:57 +0530

    info : first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+// Import Express
+const express = require('express');
+
+// Create an instance of an Express app
+const app = express();
+
+// Define a port to listen on
+const port = 3000;
+
+// Define a route handler for the default home page
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
