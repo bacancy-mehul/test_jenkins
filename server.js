a82b16d98dc396b0f8c8045f43ed16f0ba267e36
bacancy-mehul <mehul.morker@bacancy.com>
2024-08-05 10:46:19 +0530

    info: small

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-// Import Express
-const express = require('express');
+// // Import Express
+// const express = require('express');
 
-// Create an instance of an Express app
-const app = express();
+// // Create an instance of an Express app
+// const app = express();
 
-// Define a port to listen on
-const port = 3000;
+// // Define a port to listen on
+// const port = 3000;
 
-// Define a route handler for the default home page
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+// // Define a route handler for the default home page
+// app.get('/', (req, res) => {
+//   res.send('Hello, World!');
+// });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+// // Start the server
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
+
+
+console.log("server is running")
